8a45f7ad131e9119b147adb90cb619630e09353d
iBrokeTheCode <alexrtc.dev@gmail.com>
2025-08-22 10:43:03 -0500

    chore: Test main app

--- src/streamlit_app.py
+++ src/streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 
 st.title("Hello")
-name = st.text_input("Enter your name")
+name = st.text_input("Your name:")
 if st.button("Submit"):
-    st.text(f"Hello {name}")
+    st.text(f"Hello {name}, nice to meet you!")
